74fbc39b1758ed1f3661f46493e930472b93661c
Sudhir Tirumareddy <tsudhir.india@gmail.com>
2026-08-16 16:02:47 +0530

    refactor: introduce BaseTest and Page Object Model

---
+++ selenium-framework/src/test/java/com/tirumareddy/qa/WebFormPage.java
@@ -0,0 +1,82 @@
+package com.tirumareddy.qa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebFormPage {
+    private final WebDriver driver;
+
+    private final By textInput =
+            By.id("my-text-id");
+
+    private final By checkbox =
+            By.id("my-check-2");
+
+    private final By radioButton =
+            By.id("my-radio-2");
+
+    private final By dropdownLocator =
+            By.cssSelector(".form-select");
+
+    private final By submitButton =
+            By.tagName("button");
+
+    private final By message =
+            By.id("message");
+
+    public WebFormPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void selectCheckbox() {
+        driver.findElement(checkbox)
+                .click();
+    }
+
+    public void submit() {
+        driver.findElement(submitButton).click();
+    }
+
+    public String getMessage() {
+        return driver.findElement(message).getText();
+    }
+
+    public String getTextValue() {
+        return driver.findElement(textInput)
+                .getDomProperty("value");
+    }
+
+    public void selectDropdown(String option) {
+        Select select =
+                new Select(driver.findElement(dropdownLocator));
+        select.selectByVisibleText(option);
+    }
+
+    public void selectRadiButton() {
+        driver.findElement(radioButton)
+                .click();
+    }
+
+    public String getSelectedDropdownOption() {
+        Select select =
+                new Select(driver.findElement(dropdownLocator));
+        return select.getFirstSelectedOption().getText();
+    }
+
+    public void enterText(String value) {
+        driver.findElement(textInput)
+                .sendKeys(value);
+    }
+
+    public boolean isCheckboxSelected() {
+        return driver.findElement(checkbox)
+                .isSelected();
+    }
+
+    public boolean isRadiButtonSelected() {
+        return driver.findElement(radioButton)
+                .isSelected();
+    }
+}
--- selenium-framework/src/test/java/com/tirumareddy/qa/WebFormTest.java
+++ selenium-framework/src/test/java/com/tirumareddy/qa/WebFormTest.java
@@ -1,90 +1,71 @@
 package com.tirumareddy.qa;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class WebFormTest extends BaseTest {
+    private WebFormPage webFormPage;
+
+    @BeforeEach
+    void initializePage() {
+        webFormPage = new WebFormPage(driver);
+    }
 
     @Test
     void shouldEnterText() {
 
-        WebElement textInput =
-                driver.findElement(By.xpath("//input[@id='my-text-id']"));
-
-        textInput.sendKeys("Selenium Framework");
+        webFormPage.enterText("Selenium Framework");
 
         assertEquals(
                 "Selenium Framework",
-                textInput.getDomProperty("value")
+                webFormPage.getTextValue()
         );
     }
 
     @Test
     void shouldSelectCheckbox() {
 
-        WebElement checkbox =
-                driver.findElement(By.cssSelector("#my-check-2"));
+        assertFalse(webFormPage.isCheckboxSelected());
 
-        assertFalse(checkbox.isSelected());
+        webFormPage.selectCheckbox();
 
-        checkbox.click();
-
-        assertTrue(checkbox.isSelected());
+        assertTrue(webFormPage.isCheckboxSelected());
     }
 
     @Test
     void shouldSelectRadioButton() {
 
-        WebElement radioButton =
-                driver.findElement(By.id("my-radio-2"));
+        assertFalse(webFormPage.isRadiButtonSelected());
 
-        assertFalse(radioButton.isSelected());
+        webFormPage.selectRadiButton();
 
-        radioButton.click();
-
-        assertTrue(radioButton.isSelected());
+        assertTrue(webFormPage.isRadiButtonSelected());
     }
 
     @Test
     void shouldSelectDropdownOption() {
 
-        WebElement dropdownElement =
-                driver.findElement(By.cssSelector(".form-select"));
-
-        Select dropdown = new Select(dropdownElement);
-
-        dropdown.selectByVisibleText("Two");
-
+        webFormPage.selectDropdown("Two");
         assertEquals(
                 "Two",
-                dropdown.getFirstSelectedOption().getText()
+                webFormPage.getSelectedDropdownOption()
         );
     }
 
     @Test
     void shouldSubmitFormSuccessfully() {
+        webFormPage.enterText("Sudhir");
 
-        WebElement textInput =
-                driver.findElement(By.name("my-text"));
-
-        textInput.sendKeys("Sudhir");
-
-        WebElement submitButton =
-                driver.findElement(By.tagName("button"));
-
-        submitButton.click();
+        webFormPage.submit();
 
         WebDriverWait wait = new WebDriverWait(driver,
                 Duration.ofSeconds(5));
@@ -97,13 +78,9 @@ public class WebFormTest extends BaseTest {
                 ExpectedConditions.visibilityOfElementLocated(By.id("message"))
         );
 
-        WebElement message =
-                driver.findElement(By.id("message"));
-
         assertEquals(
                 "Received!",
-                message.getText()
+                webFormPage.getMessage()
         );
-
     }
 }
